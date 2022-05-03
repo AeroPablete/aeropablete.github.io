96ffefb81cc8a04ad08e54d3c550bfab0e2e0ce1
ONATE VILLAVERDE <pablojavier.onatevillaverde@cgi.com>
2022-05-03 10:39:13 -0400

    Ajout Tooltips

--- index.js
+++ index.js
@@ -211,6 +211,11 @@ function setEventBindings() {
     
     document.getElementById("search_reset")
             .addEventListener("click", resetDriverSearch)
+
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 }
 
 /**
